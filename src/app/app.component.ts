f4d12b6dc4251a6ff4a041d547ccdef3e1ca9bcc
ameyabhasme <ameyabhasme5@gmail.com>
2021-10-24 15:35:31 +0530

    Modified App component to display Interpolation(One way) Data Binding using template expression

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'helloworld';
+  title = 'HelloWorld';
+
+  ngOnInit(): void{
+    this.title = "Hello from BridgeLabz!"
+  }
 }
